fefe88c8650c79c58e4e1027afe1c28d1160fc7e
fliyyer <rahmatcube15@gmail.com>
2022-11-15 09:42:31 +0700

    mamat

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,31 +1,33 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from 'react'
-import NavbarLogin from '../components/NavbarLogin'
+import Navigation from '../components/Navigation'
 import LoginImg from '../assets/login.png'
 
-export default class LoginPage extends Component {
+export default class RegisterPage extends Component {
   render() {
     return (
       <div>
-        <NavbarLogin />
+        <Navigation />
         <main>
             <div className='login-page'>
+            <div className='hero-login'>
+                    <img src={LoginImg}/>
+                </div>
                 <div>
                     <form>
-                      <div className="login-info">
-                      <h2>Login</h2>
+                      <div className="register-info">
+                      <h2>Register</h2>
                         <label><b>Username</b></label>
                         <input type="text" placeholder="Username" name="uname" required/>
                         <label><b>Password</b></label>
                         <input type="password" placeholder="Password" name="psw" required/>
-                        <button className="button-login"type="submit">Login</button>
-                        <p className='info-login'>Belum Punya akun? Silahkan <a href="/">Register</a></p>
+                        <label><b>Confirm Password</b></label>
+                        <input type="password" placeholder="Confirm Password" name="psw" required/>
+                        <button className="button-login"type="submit">Register</button>
+                        <p className='info-login'>Sudah Punya akun? Silahkan <a href="/">Login</a></p>
                       </div>
                     </form>
-                </div>
-                <div className='hero-login'>
-                    <img src={LoginImg}/>
                 </div>
             </div>
         </main>
