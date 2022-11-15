459d182b2ab2cc7d43806f3e8546284c4cfe8f2a
ngabulil <yogaulilulul@gmail.com>
2022-11-15 13:47:31 +0700

    aaaa

---
+++ src/components/HomepageItem/ArticleButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ArticleButton extends Component {
+  render() {
+    return (
+      <div>
+        <button>Mulai</button>
+      </div>
+    )
+  }
+}
+
