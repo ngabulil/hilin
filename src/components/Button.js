63292580b2eda9cdff3e901ded60b9911a8aafe7
fliyyer <rahmatcube15@gmail.com>
2022-11-23 10:52:19 +0700

    authentication

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,3 +9,5 @@ export default class GetStartBtn extends Component {
     )
   }
 }
+
+
--- src/components/HomepageItem/MentalTestDetail.js
+++ src/components/HomepageItem/MentalTestDetail.js
@@ -4,15 +4,15 @@ import Button from '../Button';
 
 export default function MentalTestDetail() {
   return (
-    <div className='mentaltest-detail'> 
-        <div className='mentaltest-detail-content'>
-            <h1>Mental Test</h1>
-            <p>Rumah sakit adalah bagian integral dari suatu organisasi sosial dan kesehatan dengan fungsi menyediakan pelayanan paripurna (komprehensif).</p>
-            <Button name='Mulai Tes'/>
-        </div>     
-        <div className='mentaltest-detail-img'>
-            <img src={MentalDetailIcon} alt='Article Icon'></img>
-        </div>           
+    <div className='mentaltest-detail'>
+      <div className='mentaltest-detail-content'>
+        <h1>Mental Test</h1>
+        <p>Rumah sakit adalah bagian integral dari suatu organisasi sosial dan kesehatan dengan fungsi menyediakan pelayanan paripurna (komprehensif).</p>
+        <Button name='Mulai Tes' />
+      </div>
+      <div className='mentaltest-detail-img'>
+        <img src={MentalDetailIcon} alt='Article Icon'></img>
+      </div>
     </div>
   )
 }
