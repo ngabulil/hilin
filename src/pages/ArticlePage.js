0fb9e0debb7f91df00f44e884eacbe4426c049f8
ngabulil <yogaulilulul@gmail.com>
2022-11-26 19:07:27 +0700

    loading

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -5,10 +5,12 @@ import Button from "../components/Button";
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../config/Firebase";
+import { Bars } from "react-loader-spinner";
 
 function ArticlePage() {
   const [articlenew, setArticlenew] = useState([]);
   const [articlepop, setArticlepop] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const articlePop = async () => {
@@ -23,50 +25,93 @@ function ArticlePage() {
         getArticlenew.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
       );
     };
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
     articlePop();
     articleNew();
   }, []);
   return (
     <div className="article-page">
       <Navigation />
-      <div className="article-page-container">
-        {articlepop.map((data) => (
-          <main className="article-header-container" key={data.id}>
-            <h1 className="article-header-container-h1" data-aos="fade-down" data-aos-duration="1500">Article Terpopuler</h1>
-            <div className="article-header">
-              <div className="article-hero" data-aos="fade-right" data-aos-duration="1500">
-                <img src={data.img} alt="article terbaru" />
+      {loading ? (
+        <div className="bar-load">
+          <Bars
+            height="150"
+            width="150"
+            radius="9"
+            color="var(--primary-color)"
+            ariaLabel="loading"
+            wrapperStyle
+            wrapperClass
+          />
+        </div>
+      ) : (
+        <>
+          <div className="article-page-container">
+            {articlepop.map((data) => (
+              <main className="article-header-container" key={data.id}>
+                <h1
+                  className="article-header-container-h1"
+                  data-aos="fade-down"
+                  data-aos-duration="1500"
+                >
+                  Article Terpopuler
+                </h1>
+                <div className="article-header">
+                  <div
+                    className="article-hero"
+                    data-aos="fade-right"
+                    data-aos-duration="1500"
+                  >
+                    <img src={data.img} alt="article terbaru" />
+                  </div>
+                  <div
+                    className="article-description"
+                    data-aos="fade-left"
+                    data-aos-duration="1500"
+                  >
+                    <h1>{data.title}</h1>
+                    <p>
+                      {data.desc}
+                      <a href={data.link} target="blank">
+                        Selengkapnya
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </main>
+            ))}
+            <div className="article-header-main">
+              <div
+                className="article-hero"
+                data-aos="fade-up"
+                data-aos-duration="1500"
+              >
+                <h1>Article Terbaru</h1>
               </div>
-              <div className="article-description" data-aos="fade-left" data-aos-duration="1500">
-                <h1>{data.title}</h1>
-                <p>
-                  {data.desc}
-                  <a href={data.link} target="blank">
-                    Selengkapnya
-                  </a>
-                </p>
+              <div className="article-main-card-wrap">
+                {articlenew.map((data) => (
+                  <div
+                    className="article-main-card"
+                    key={data.id}
+                    data-aos="fade-up"
+                    data-aos-duration="1500"
+                  >
+                    <img src={data.img} alt="viva icon" />
+                    <p>{data.title}</p>
+                    <a href={data.link} target="blank">
+                      <Button name="Selengkapnya" />
+                    </a>
+                  </div>
+                ))}
               </div>
             </div>
-          </main>
-        ))}
-        <div className="article-header-main">
-          <div className="article-hero" data-aos="fade-up" data-aos-duration="1500">
-            <h1>Article Terbaru</h1>
           </div>
-          <div className="article-main-card-wrap">
-            {articlenew.map((data) => (
-              <div className="article-main-card" key={data.id} data-aos="fade-up" data-aos-duration="1500">
-                <img src={data.img} alt="viva icon" />
-                <p>{data.title}</p>
-                <a href={data.link} target="blank">
-                  <Button name="Selengkapnya" />
-                </a>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <Footer />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
